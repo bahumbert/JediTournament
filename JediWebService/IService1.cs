6bba1f21b2446f16687d6a7c5035bb3850b40bf4
simondujardin <dujardin.simon@yahoo.fr>
2016-02-19 15:02:42 +0100

    Ajout WebService

---
+++ JediWebService/IService1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JediWebService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<JediContract> GetJedis();
+
+        [OperationContract]
+        List<StadeContract> GetStades();
+
+        [OperationContract]
+        List<MatchContract> GetMatchs();
+
+        [OperationContract]
+        List<TournoiContract> GetTournois();
+
+        [OperationContract]
+        List<CaracteristiqueContract> GetCaracteristiquesByJedi(JediContract j);
+
+        [OperationContract]
+        void AddJedi(string nom, bool isSith, CaracteristiqueContract force, CaracteristiqueContract defense, CaracteristiqueContract chance, CaracteristiqueContract sante);
+    }
+    [DataContract]
+    public class JediContract
+    {
+        string nom;
+        List<CaracteristiqueContract> caracteristiques;
+        bool isSith;
+
+        [DataMember]
+        public List<CaracteristiqueContract> Caracteristiques
+        {
+            get { return caracteristiques; }
+            set { caracteristiques = value; }
+        }
+        [DataMember]
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        [DataMember]
+        public bool IsSith
+        {
+            get { return isSith; }
+            set { isSith = value; }
+        }
+    }
+    [DataContract]
+    public class StadeContract
+    {
+        int nbPlace;
+        string planete;
+        [DataMember]
+        public int NbPlace
+        {
+            get { return nbPlace; }
+            set { nbPlace = value; }
+        }
+
+        [DataMember]
+        public string Planete
+        {
+            get { return planete; }
+            set { planete = value; }
+        }
+    }
+    [DataContract]
+    public class MatchContract
+    {
+        int id;
+        int idJediVainqueur;
+        JediContract jedi1;
+        JediContract jedi2;
+        StadeContract stade;
+
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public int IdJediVainqueur
+        {
+            get { return idJediVainqueur; }
+            set {idJediVainqueur = value; }
+        }
+
+        [DataMember]
+        public JediContract Jedi1
+        {
+            get { return jedi1; }    
+            set { jedi1 = value; }
+        }
+
+        [DataMember]
+        public JediContract Jedi2
+        {
+            get { return jedi2; }
+            set { jedi2 = value; }
+        }
+
+        [DataMember]
+        public StadeContract Stade
+        {
+            get { return stade; }
+            set { stade = value; }
+        }
+    }
+    [DataContract]
+    public class TournoiContract
+    {
+        List<MatchContract> matchs;
+        string nom;
+
+        [DataMember]
+        public List<MatchContract> Matchs
+        {
+            get { return matchs; }
+            set { matchs = value; }
+        }
+
+        [DataMember]
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+    }
+    [DataContract]
+    public class CaracteristiqueContract
+    {
+        string nom;
+        int valeur;
+
+        [DataMember]
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        [DataMember]
+        public int Valeur
+        {
+            get { return valeur; }
+            set { valeur = value; }
+        }
+    }
+}
---
+++ JediWebService/Service1.svc.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntitiesLayer;
+using BusinessLayer;
+
+namespace JediWebService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public List<JediContract> GetJedis()
+        {
+            List<JediContract> list = new List<JediContract>();
+            List<Jedi> listjedi = new List<Jedi>();
+            JediTournamentManager jtm = new JediTournamentManager();
+            listjedi = jtm.getAllJedis();
+            foreach(Jedi j in listjedi)
+            {
+                JediContract jedi = new JediContract();
+                jedi.Nom = j.Nom;
+                jedi.IsSith = j.IsSith;
+                jedi.Caracteristiques = new List<CaracteristiqueContract>();
+                CaracteristiqueContract force = new CaracteristiqueContract();
+                CaracteristiqueContract sante = new CaracteristiqueContract();
+                CaracteristiqueContract defense = new CaracteristiqueContract();
+                CaracteristiqueContract chance = new CaracteristiqueContract();
+
+                force.Nom = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Force).Nom;
+                force.Valeur = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Force).Valeur;
+                jedi.Caracteristiques.Add(force);
+                chance.Nom = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Chance).Nom;
+                chance.Valeur = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Chance).Valeur;
+                jedi.Caracteristiques.Add(chance);
+                defense.Nom = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Defense).Nom;
+                defense.Valeur = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Defense).Valeur;
+                jedi.Caracteristiques.Add(defense);
+                sante.Nom = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Sante).Nom;
+                sante.Valeur = j.Caracteristiques.Find(x => x.Definition == EDefCaracteristique.Sante).Valeur;
+                jedi.Caracteristiques.Add(sante);
+                list.Add(jedi);
+            }
+            return list;
+         }
+        public List<StadeContract> GetStades()
+        {
+            List<StadeContract> list = new List<StadeContract>();
+            List<Stade> liststade = new List<Stade>();
+            JediTournamentManager jtm = new JediTournamentManager();
+            liststade = jtm.getAllStades();
+            foreach (Stade s in liststade)
+            {
+                StadeContract stade = new StadeContract();
+                stade.NbPlace = s.NbPlaces;
+                stade.Planete = s.Planete;
+                list.Add(stade);
+            }
+            return list;
+        }
+        public List<MatchContract> GetMatchs()
+        {
+            List<MatchContract> list = new List<MatchContract>();
+            List<Match> listmatch = new List<Match>();
+            List<JediContract> listJedi = new List<JediContract>();
+            List<StadeContract> listStade = new List<StadeContract>();
+            JediTournamentManager jtm = new JediTournamentManager();
+            listJedi = GetJedis();
+            listStade = GetStades();
+            listmatch = jtm.getAllMatchs();
+            foreach (Match m in listmatch)
+            {
+                MatchContract match = new MatchContract();
+                match.Id = m.ID;
+                match.IdJediVainqueur = m.IdJediVainqueur;
+                match.Jedi1 = listJedi.Find(x => x.Nom ==  m.Jedi1.Nom);
+                match.Jedi2 = listJedi.Find(x => x.Nom == m.Jedi2.Nom);
+                match.Stade = listStade.Find(x => x.Planete == m.Stade.Planete);
+                list.Add(match);
+            }
+            return list;
+        }
+        public List<TournoiContract> GetTournois()
+        {
+            List<TournoiContract> list = new List<TournoiContract>();
+            List<Tournoi> listtournoi = new List<Tournoi>();
+            List<MatchContract> listmatch = new List<MatchContract>();
+            JediTournamentManager jtm = new JediTournamentManager();
+            listmatch = GetMatchs();
+            listtournoi = jtm.getAllTournois();
+            foreach (Tournoi t in listtournoi)
+            {
+                TournoiContract tournoi = new TournoiContract();
+                tournoi.Matchs = new List<MatchContract>();
+                Tournoi tournoi2 = listtournoi.Find(x => x.Nom == t.Nom);
+                foreach(Match m in tournoi2.Matchs)
+                {
+                    MatchContract match = new MatchContract();
+                    match = listmatch.Find(x => x.Id == m.ID);
+                    if (match != null)
+                    {
+                        tournoi.Matchs.Add(match);
+                    }
+                }
+                tournoi.Nom = t.Nom;
+                list.Add(tournoi);
+            }
+            return list;
+        }
+        public List<CaracteristiqueContract> GetCaracteristiquesByJedi(JediContract j)
+        {
+            List<CaracteristiqueContract> list = new List<CaracteristiqueContract>();
+            List<Caracteristique> listcaract = new List<Caracteristique>();
+            List<Jedi> listjedi = new List<Jedi>();
+            JediTournamentManager jtm = new JediTournamentManager();
+            listjedi = jtm.getAllJedis();
+            Jedi jedi = listjedi.Find(x => x.Nom == j.Nom);
+            foreach(Caracteristique c in jedi.Caracteristiques)
+            {
+                CaracteristiqueContract carac = new CaracteristiqueContract();
+                carac.Nom = c.Nom;
+                carac.Valeur = c.Valeur;
+                list.Add(carac);
+            }
+            return list;
+        }
+        public void AddJedi(string nom, bool isSith, CaracteristiqueContract force, CaracteristiqueContract defense, CaracteristiqueContract chance, CaracteristiqueContract sante)
+        {
+            JediTournamentManager jtm = new JediTournamentManager();
+            List<Caracteristique> carac = new List<Caracteristique>();
+            Caracteristique force2 = new Caracteristique(1, force.Nom,EDefCaracteristique.Force,ETypeCaracteristique.Jedi,force.Valeur);
+            Caracteristique chance2 = new Caracteristique(2,chance.Nom,EDefCaracteristique.Chance, ETypeCaracteristique.Jedi, chance.Valeur);
+            Caracteristique sante2 = new Caracteristique(3,sante.Nom,EDefCaracteristique.Sante, ETypeCaracteristique.Jedi, sante.Valeur);
+            Caracteristique defense2 = new Caracteristique(4,defense.Nom,EDefCaracteristique.Defense, ETypeCaracteristique.Jedi,defense.Valeur);
+            carac.Add(force2);
+            carac.Add(defense2);
+            carac.Add(chance2);
+            carac.Add(sante2);
+            Jedi jedi = new Jedi(0,nom,isSith,carac,"");
+            jtm.addJedi(jedi);
+        }
+    }
+}
